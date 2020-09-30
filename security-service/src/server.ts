f0e369b99461d7123bdcb82d3a3b51b6aea33bcb
OlivierLardenois <olivier@gymlib.com>
2020-09-30 10:23:01 +0200

    ✨ - Add verifyUserToken route to security MS

--- security-service/src/server.ts
+++ security-service/src/server.ts
@@ -9,12 +9,27 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get<{}, {}, {}, { userId: string }>('/userToken', ({ query: { userId } }, res) => {
-  if (!userId) res.status(400).send('MISSING_USER_ID');
+  if (!userId) res.json({ ok: false, token: '' });
 
   const token = jwt.sign({ userId }, process.env.ACCESS_TOKEN_SECRET!, {
     expiresIn: '15m',
   });
-  return res.json({ token });
+  return res.json({ ok: true, token });
+});
+
+app.post<{}, {}, { token: string }>('/verifyUserToken', (req, res) => {
+  let payload;
+  try {
+    payload = jwt.verify(req.body.token, process.env.ACCESS_TOKEN_SECRET!) as {
+      userId: string;
+    };
+  } catch (error) {
+    // TODO: handle logger
+    // logger.warn('Cannot verify userToken');
+    return res.send({ ok: false, token: '' });
+  }
+
+  return res.send({ ok: true, token: payload.userId });
 });
 
 app.listen(port, () => {
